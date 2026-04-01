a94a0cfc9da7ed6d9e8004653d9ccce46f6d40a8
Rayyan <rayyan808@gmail.com>
2026-04-01 16:58:51 +0400

    update eth address and key hash

--- script/DeployETH.s.sol
+++ script/DeployETH.s.sol
@@ -8,11 +8,11 @@ import {Raffle} from "../src/Raffle.sol";
 //Sepolia subscription id:
 contract RaffleScript is Script {
     Raffle raffleContract;
-    uint256 subscriptionId = 29798300099173099017092510159386407287862520034448296410164372830033307388568;
+    uint256 subscriptionId = 53910715258455525673306227434249246930644578090699990663112838043934870643230;
     address owner = address(0x5991AA8b650D8d58a57663C18BEc57b8DD25CED9);
     address aliceToken = address(0xAC51066d7bEC65Dc4589368da368b212745d63E8);
     address vrfCoordinator = address(0xD7f86b4b8Cae7D942340FF628F82735b7a20893a);
-    bytes32 keyHash = 0xc6bf2e7b88e5cfbb4946ff23af846494ae1f3c65270b79ee7876c9aa99d3d45f;
+    bytes32 keyHash = 0x3fd2fec10d06ee8f65e7f2e95f5c56511359ece3f33960ad8a866ae24a8ff10b;
     uint32 callbackGasLimit = 100_000;
     uint16 requestConfirmations = 3;
     uint32 houseFee = 1000; // 10%
