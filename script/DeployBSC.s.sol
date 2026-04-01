fa021bc33bbc1445771e55fcacd604bc4f5c51db
Rayyan <rayyan808@gmail.com>
2026-04-01 17:15:06 +0400

    update bsc contract and keyhash

--- script/DeployBSC.s.sol
+++ script/DeployBSC.s.sol
@@ -8,14 +8,14 @@ import {Raffle} from "../src/Raffle.sol";
 
 contract RaffleScript is Script {
     Raffle raffleContract;
-    uint256 subscriptionId = 96349751094655264643182161929922189477016467088195930821319915882799913221132;
+    uint256 subscriptionId = 54148331197336958396352205331393564549434545544171579606529891119239243085239;
     address owner = address(0x5991AA8b650D8d58a57663C18BEc57b8DD25CED9);
     address aliceToken = address(0xAC51066d7bEC65Dc4589368da368b212745d63E8);
     address vrfCoordinator = address(0xd691f04bc0C9a24Edb78af9E005Cf85768F694C9);
-    bytes32 keyHash = 0xb94a4fdb12830e15846df59b27d7c5d92c9c24c10cf6ae49655681ba560848dd;
+    bytes32 keyHash = 0x130dba50ad435d4ecc214aad0d5820474137bd68e7e77724144f27c3c377d3d4;
     uint32 callbackGasLimit = 100_000;
     uint16 requestConfirmations = 3;
-    uint32 houseFee = 1000; // 10%
+    uint32 houseFee = 5000; // 50%
 
     function setUp() public {}
 
